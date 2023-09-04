3d138c98898f3eac75c423aa11957334227b7729
Mouad-marah <mouadmarah0@gmail.com>
2023-09-04 22:32:15 +0000

    print all

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "list.h"
+/**
+*check_cycle - function declaration
+*@list: list pointer
+*Return: 0 always SUCCESS
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *faster;
+	
+	first = list->next;
+	faster = list->next->next;
+	if (list == NULL || list->next == NULL)
+		return (0);
+	while (faster && first && faster->next)
+	{
+		if (first == faster)
+			return (1);
+		first = first->next;
+		faster = faster->next->next
+	}
+	return (0);
+}
